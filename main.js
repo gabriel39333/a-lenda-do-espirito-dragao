a32c964e75183aecbaecb844897c82c041e827e5
gabriel39333 <gabriel.leite.rodrigues@escola.pr.gov.br>
2025-10-29 10:08:07 -0300

    inico

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
-const avanca =document.querySelectorAll(".btn-proximo")
+const avanca =document.querySelectorAll('.btn-proximo')
 console.log(avanca)
+avanca.forEach(button=> {
+    button.addEventListener('click',function(){
+        const atual = document.querySelector('.ativo')
+    })
+})
